c72f657f9f4d1303fccca185969333932adfab45
nipunrajk <nipunraj99@gmail.com>
2022-10-01 16:04:59 +0530

    initial styling

---
+++ src/CurrencyRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const CurrencyRow = () => {
+  return (
+    <div>
+      <input type='number' className='input'></input>
+      <select>
+        <option value='hi'>HI</option>
+      </select>
+    </div>
+  )
+}
+
+export default CurrencyRow
