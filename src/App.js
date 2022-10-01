a0773f23eb2368349b3cccb7c3e32f8a910eb6d3
nipunrajk <nipunraj99@gmail.com>
2022-10-01 23:37:12 +0530

    selecting option from dropdown

--- src/App.js
+++ src/App.js
@@ -5,21 +5,34 @@ import { BASE_URL, requestOptions } from './Urls'
 
 function App() {
   const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
 
   useEffect(() => {
     fetch(BASE_URL, requestOptions)
       .then((response) => response.json())
       .then((data) => {
+        const firstCurrency = Object.keys(data.rates)[0]
         setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+        setFromCurrency(data.base)
+        setToCurrency(firstCurrency)
       })
   }, [])
 
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+      />
       <div className='equals'>=</div>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+      />
     </>
   )
 }
--- src/CurrencyRow.jsx
+++ src/CurrencyRow.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const CurrencyRow = (props) => {
-  const { currencyOptions } = props
+  const { currencyOptions, selectCurrency, onChangeCurrency } = props
   return (
     <div>
       <input type='number' className='input'></input>
-      <select>
+      <select value={selectCurrency} onChange={onChangeCurrency}>
         {currencyOptions.map((option, idx) => (
           <option key={idx} value={option}>
             {option}
